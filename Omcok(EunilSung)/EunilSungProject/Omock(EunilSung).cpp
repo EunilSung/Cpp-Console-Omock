c8ea77da2dbed967cee47d7b5371f3ff5489f9c4
EunilSung <39809020+EunilSung@users.noreply.github.com>
2020-06-23 10:21:51 +0900

    Add files via upload
    final update

--- Omcok(EunilSung)/EunilSungProject/Omock(EunilSung).cpp
+++ Omcok(EunilSung)/EunilSungProject/Omock(EunilSung).cpp
@@ -7,7 +7,6 @@ int main()
     const int BACKGROUND_WITH = 19;
     const int BACKGROUND_HIGHT = 19;
     bool isColor = true;
-    bool isGameOver = false;
     int x, y;
     string bord[BACKGROUND_HIGHT][BACKGROUND_WITH];
     for (int i = 0; i < BACKGROUND_HIGHT; i++) {
@@ -59,15 +58,16 @@ int main()
             }
             cout << endl;
         }
-        if (isGameOver)
-            return 0;
-
+      
         isColor ? cout << "●'s X,Y: " : cout << "○'s X,Y: ";
         cin >> x >> y;
-        if (x > 9 || y > 9)
+        if (x == 99 && y == 99)
+            return 0;
+        else if (x > 19 || y > 19)
             continue;
+
         x--; y--;
-        if (bord[y][x] == "●"|| bord[y][x] == "○")
+        if (bord[y][x] == "●" || bord[y][x] == "○")
             continue;
 
         if (isColor) {
@@ -77,28 +77,6 @@ int main()
         else {
             bord[y][x] = "○";
             isColor = true;
-        }
-        for (int i = 0; i < BACKGROUND_HIGHT; i++) {
-            for (int j = 0; j < BACKGROUND_WITH; j++) {
-                if (bord[i][j] == "●" || bord[i][j] == "○") {
-                    if (bord[i][j] == bord[i + 1][j] && bord[i][j] == bord[i + 2][j]
-                        && bord[i][j] == bord[i + 3][j] && bord[i][j] == bord[i + 4][j]) {
-                        isGameOver = true;
-                    }
-                    if (bord[i][j] == bord[i][j + 1] && bord[i][j] == bord[i][j + 2]
-                        && bord[i][j] == bord[i][j + 3] && bord[i][j] == bord[i][j + 4]) {
-                        isGameOver = true;
-                    }
-                    if (bord[i][j] == bord[i + 1][j + 1] && bord[i][j] == bord[i + 2][j + 2]
-                        && bord[i][j] == bord[i + 3][j + 3] && bord[i][j] == bord[i + 4][j + 4]) {
-                        isGameOver = true;
-                    }
-                    if (bord[i][j] == bord[i + 1][j - 1] && bord[i][j] == bord[i + 2][j - 2]
-                        && bord[i][j] == bord[i + 3][j - 3] && bord[i][j] == bord[i + 4][j - 4]) {
-                        isGameOver = true;
-                    }
-                }
-            }
         }
     }
 }
